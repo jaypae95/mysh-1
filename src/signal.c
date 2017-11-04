e5faf8bc114c78b238a1656a0c64cceb6b0bfa24
jaypae95 <jaypae95@naver.com>
2017-11-04 13:36:44 +0900

    still tryin...why signal not workin(make not workin)

--- src/signal.c
+++ src/signal.c
@@ -3,9 +3,12 @@
 void catch_sigint(int)
 {
   // TODO: File this!
+  printf("11111");
+  signal(SIGINT, SIG_IGN);
 }
 
 void catch_sigtstp(int);
 {
   // TODO: File this!
+  signal(SIGTSTP, SIG_IGN);
 }
